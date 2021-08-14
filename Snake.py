a0122ca6e260c5e4cd5d7bce55163a1a0d99fe40
Kevin Olanday <kevin.olanday@gmail.com>
2021-08-14 19:34:02 +0800

    Add game over screen

--- Snake.py
+++ Snake.py
@@ -6,6 +6,59 @@ from curses import KEY_UP, KEY_DOWN, KEY_RIGHT, KEY_LEFT, wrapper
 from random import randint
 
 
+class Score:
+    def __init__(self, value = 0):
+        self.value = value
+    
+    def add(self, points):
+        self.value += points
+    
+class Snake:
+    def __init__(self, coordinates):
+        self.coordinates = coordinates
+        self.length = len(coordinates)
+        self.head_coordinates = list(coordinates[0])
+        self.speed = 1
+
+    def __repr__(self):
+        return f"Snake at coordinates {self.coordinates} with length {self.length}"
+
+    def move(self, direction):
+        self.head_coordinates[0] += (direction == "down" and 1) + (direction == "up" and -1)      
+        self.head_coordinates[1] += (direction == "left" and -1) + (direction == "right" and 1)
+        self.coordinates.insert(0, [self.head_coordinates[0], self.head_coordinates[1]])        
+        if self.head_coordinates != food.coordinates:
+            head = self.coordinates.pop()
+            window.addch(head[0], head[1], ' ')
+        window.addch(self.head_coordinates[0], self.head_coordinates[1], '#', curses.color_pair(1))
+
+    def set_speed(self, speed = 1):
+        print(speed)
+        window.timeout(150 - speed)
+        
+    def eat_food(self, food):
+        print("nom!")
+        self.length += 1
+        self.set_speed(self.length)
+        if food.type == "Normal":
+            window2.addch(12,19,">", curses.color_pair(1))
+
+class Food:
+    def __init__(self, coordinates, type = "Normal"):
+        self.coordinates = coordinates
+        self.type = type
+        self.display()
+
+    def __repr__(self):
+        return f"{self.type} food at {self.coordinates}"
+
+    def display(self):
+        window.addch(self.coordinates[0], self.coordinates[1], '○', curses.color_pair(2)) 
+
+    def respawn(self):
+        while self.coordinates in snake.coordinates:
+           self.coordinates = [randint(1, 18), randint(1, 28)]
+        self.display()
 
 # initialize the game window 
 curses.initscr()  # initialize screen
@@ -18,11 +71,12 @@ curses.curs_set(0)  # hide the cursor
 window = curses.newwin(20, 30, 0, 30)  # create game window
 window.keypad(True)  # enable keypad
 window.nodelay(True)  # do not wait for the user input
+window.timeout(150)
 
 # initialize the instructions window
 window2 = curses.newwin(20, 30, 0, 0)  # create game window
 window2.border(0)
-window2.addstr(0, 11, ' SNAKE ',  curses.color_pair(1))
+window2.addstr(0, 10, '[ SNAKE ]',  curses.color_pair(1))
 window2.addstr(5, 3,"Press ↑, ↓, →, ← to move")
 window2.addstr(7, 3,"Press ENTER to start game")
 window2.addstr(8, 4,"Press ESC to exit game")
@@ -33,65 +87,11 @@ window2.addstr(15,4," `-...-'", curses.color_pair(1))
 
 # initialize global variables
 key = ""
-
-# initialize first food and snake coordinates
-snake = [[5, 8], [5, 7], [5, 6]]
-food = [10, 15]
-
-# display the first food
-window.addch(food[0], food[1], '○', curses.color_pair(2))
-
-class Score:
-    def __init__(self, value = 0):
-        self.value = value
-    
-    def add(self, points):
-        self.value += points
-    
 score = Score()
 
-class Snake:
-    def __init__(self, coordinates):
-        self.coordinates = coordinates
-        self.length = len(coordinates)
-        self.head_coordinates = list(coordinates[0])
-
-    def __repr__(self):
-        return f"Snake at coordinates {self.coordinates} with length {self.length}"
-
-    def move(self, direction):
-        self.head_coordinates[0] += (direction == "down" and 1) + (direction == "up" and -1)      
-        self.head_coordinates[1] += (direction == "left" and -1) + (direction == "right" and 1)
-        self.coordinates.insert(0, [self.head_coordinates[0], self.head_coordinates[1]])
-        
-        if self.head_coordinates != food2.coordinates:
-            head = self.coordinates.pop()
-            window.addch(head[0], head[1], ' ')
-        window.addch(self.head_coordinates[0], self.head_coordinates[1], '#', curses.color_pair(1))
-        
-    def eat_food(self, food):
-        if food.type == "Normal":
-            window2.addch(12,19,">", curses.color_pair(1))
-
-class Food:
-    def __init__(self, coordinates, type = "Normal"):
-        self.coordinates = coordinates
-        self.type = type
-
-    def __repr__(self):
-        return f"{self.type} food at {self.coordinates}"
-
-    def display(self):
-        window.addch(self.coordinates[0], self.coordinates[1], '○', curses.color_pair(2)) 
-
-    def respawn(self):
-        while self.coordinates in snake2.coordinates:
-           self.coordinates = [randint(1, 18), randint(1, 28)]
-        self.display()
-
-snake2 = Snake([[10, 8], [10, 7], [10, 6]])
-food2 = Food([18, 15])
-food2.display()
+# initialize first food and snake coordinates
+snake = Snake([[5, 8], [5, 7], [5, 6]])
+food = Food([18, 15])
 
 
 while key != 10 and key != 27: # While the user hasn't started the game
@@ -101,15 +101,11 @@ while key != 10 and key != 27: # While the user hasn't started the game
 
 key = KEY_RIGHT
   
-counter = 0
 while key != 27:  # While they Esc key is not pressed
-
     window.border(0)
     # display the score and title
     window2.addstr(19, 11, 'Score: ' + str(score.value) + ' ', curses.color_pair(2))
     window2.refresh()
-    # make the snake faster as it eats more
-    window.timeout(140 - (int(len(snake)/5) + int(len(snake)/10)) % 120)
     # refreshes the screen and then waits for the user to hit a key
     event = window.getch()
     key = key if event == -1 else event
@@ -125,19 +121,20 @@ while key != 27:  # While they Esc key is not pressed
     elif key == KEY_RIGHT:
         direction = "right"
     
-    snake2.move(direction)
+    snake.move(direction)
 
-    # Exit if snake crosses the boundaries
-    if snake2.head_coordinates[0] == 0 or snake2.head_coordinates[0] == 19 or snake2.head_coordinates[1] == 0 or snake2.head_coordinates[1] == 29:
-        break
-    # Exit if snake runs over itself
-    if snake2.head_coordinates in snake2.coordinates[1:]:
+    # Exit if snake crosses the boundaries or runs over itself
+    if snake.head_coordinates[0] == 0 or snake.head_coordinates[0] == 19 or snake.head_coordinates[1] == 0 or snake.head_coordinates[1] == 29 or snake.head_coordinates in snake.coordinates[1:]:
+        window.addstr(9, 10,"GAME OVER!", curses.color_pair(3))
+        while key != 27:
+            event = window.getch()
+            key = key if event == -1 else event
         break
 
-    if snake2.head_coordinates == food2.coordinates:
-        snake2.eat_food(food2)
+    # If snake hits food
+    if snake.head_coordinates == food.coordinates:
+        snake.eat_food(food)
         score.add(1)
-        food2.respawn()
-
-curses.endwin()  # close the window and end the game
+        food.respawn()
+#curses.endwin()  # close the window and end the game
 print("\nScore: " + str(score.value))
